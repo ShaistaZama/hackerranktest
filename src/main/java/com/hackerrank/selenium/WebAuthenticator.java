b523445ab8eea906bea10b58a96fb47977b65e5a
Shaz <43005757+ShaistaZama@users.noreply.github.com>
2021-12-08 10:33:46 +0530

    Update WebAuthenticator.java

--- src/main/java/com/hackerrank/selenium/WebAuthenticator.java
+++ src/main/java/com/hackerrank/selenium/WebAuthenticator.java
@@ -6,6 +6,11 @@ public class WebAuthenticator {
 
     public static String authenticateAndGetText(WebDriver driver, String pageUrl) {
         // complete implementation, authentication and return body text.
-        return "";
+        driver.get("https://www.hackerrank.com/auth/login");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        Alert a=driver.switchTo().alert();
+        System.out.println(a.getText());
+        return a;
     }
 }
